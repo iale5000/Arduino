06a7a89850d16133b5259211fe3f82a70f524904
Iale Botelho Almeida <iale1000@gmail.com>
2017-10-25 10:27:31 -0300

    Add files via upload

---
+++ Potenciometro.ino
@@ -0,0 +1,17 @@
+const int pinoP= A0;
+const int led = 13;
+int valorPot = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  valorPot = analogRead( pinoP );
+  digitalWrite(led, HIGH);
+  delay(valorPot);
+  digitalWrite(led, LOW);
+  delay(valorPot);
+}
---
+++ temperatura.ino
@@ -0,0 +1,24 @@
+const int LED[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+const int LM35 = A0;
+float temperatura = 0;
+int ADClido = 0;
+const int buzzer = 13;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  analogReference(INTERNAL);
+  pinMode(buzzer, OUTPUT);
+  for (int i = 0; i < 10; i++) {
+    pinMode(LED[i], OUTPUT);
+}
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  ADClido = analogRead(LM35);
+  temperatura = ADClido * 0.1075268817204301;
+  Serial.print("Temperatura = ");
+  Serial.print(temperatura);
+  Serial.println(" *C");
+  digitalWrite(LED[0], HIGH);
+  delay(1000);
+}
